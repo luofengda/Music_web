ec0aa2869a0ec9918f46d03c267d6823ab7e3b4c
luofengda <362861693@qq.com>
2017-08-20 00:48:33 +0800

    初始化项目文件。修改vue-cli默认配置、新增字体图标、默认的样式

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <h1>初始化项目</h1>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'app'
-}
+<script type="text/ecmascript-6">
+
 </script>
 
-<style>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+// @import "~common/stylus/variable"
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: $color-theme;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App";
+// import router from './router'
+import "common/stylus/index.styl"
+// E:\github\src\common\stylus\index.styl
+// Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
-})
+  el: "#app",
+  // router,
+  render: h => h(App)
+});
