2a5d9a7df2f7c81b80ad69b379c2d9f4c9a525f8
luofengda <362861693@qq.com>
2017-08-20 15:27:03 +0800

    使用pormise来封装jsonp

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,22 @@
+import originJSOPM from "jsonp";
+
+export default function jsonp(url, data, option) {
+  url += (url.indexOf("?") < 0 ? "?" : "&") + param(data);
+  return new Promise((resolve, reject) => {
+    originJSOPM(url, option, (err, data) => {
+      if (!err) {
+        resolve(data);
+      } else {
+        reject(err);
+      }
+    });
+  });
+}
+function param(data) {
+  let url = "";
+  for (var key in data) {
+    let value = data[k] !== undefined ? data[k] : "";
+    url += `&${k}=${encodeURLComponent(value)}`;
+  }
+  return url ? url.substring(1) : "";
+}
